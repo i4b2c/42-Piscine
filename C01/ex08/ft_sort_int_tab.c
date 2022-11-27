7b2c12608171df793a18dc335b573650056dd86d
i4b2c <117470349+i4b2c@users.noreply.github.com>
2022-11-27 05:40:33 +0000

    ✔️ OK ✔️

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -39,28 +39,3 @@ void	ft_sort_int_tab(int *tab, int size)
 		i++ ;
 	}
 }
-/*
-#include <stdio.h>
-
-void putarr(int arr[], int size)
-{
-    for (int i = 0; i < (size -1); i++)
-        printf("%d, ", arr[i]);
-    printf("%d\n", arr[size - 1]);
-}
-
-int		main (void)
-{
-	int tab[] = {10, 7, 8, 9, 1, 5};
-	int size;
-	
-	size = 6;
-
-	printf("Original: ");
-	putarr(tab, size);
-
-	ft_sort_int_tab(tab, size);
-
-	printf("Modified: ");
-	putarr(tab, size);
-}*/
