364b1cfaee6e239dbb6663dc12cb2a8f981bbee9
i4b2c <117470349+i4b2c@users.noreply.github.com>
2022-11-27 05:55:29 +0000

    ✔️ OK ✔️

--- C02/ex07/ft_strupcase.c
+++ C02/ex07/ft_strupcase.c
@@ -10,20 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 char	*ft_strupcase(char *str)
 {
 	int	i;
 
-	i = 0;
-	while (str[i] != '\0')
+	i = -1;
+	while (str[++i] != '\0')
 	{
 		if (str[i] <= 'z' && str[i] >= 'a')
-		{
 			str[i] -= 32;
-		}
-		i++;
 	}
 	return (str);
 }
