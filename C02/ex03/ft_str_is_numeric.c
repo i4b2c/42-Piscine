0b34ee2492bc58e3a112fca4b65a3b398732c127
i4b2c <117470349+i4b2c@users.noreply.github.com>
2022-11-27 05:51:44 +0000

    ✔️ OK ✔️

--- C02/ex03/ft_str_is_numeric.c
+++ C02/ex03/ft_str_is_numeric.c
@@ -10,19 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
 int	ft_str_is_numeric(char *str)
 {
 	int	i;
 
-	i = 0;
-	while (str[i])
+	i = -1;
+	while (str[++i])
 	{
 		if (!(str[i] <= '9' && str[i] >= '0'))
-		{
 			return (0);
-		}
-	i++;
 	}
 	return (1);
 }
