0f898aa67d720abfe79c8d663f269581042a9259
i4b2c <117470349+i4b2c@users.noreply.github.com>
2022-11-27 05:52:48 +0000

    ✔️ OK ✔️

--- C02/ex05/ft_str_is_uppercase.c
+++ C02/ex05/ft_str_is_uppercase.c
@@ -10,19 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
 int	ft_str_is_uppercase(char *str)
 {
 	int	i;
 
-	i = 0;
+	i = -1;
 	while (str[i])
 	{
 		if (!(str[i] >= 'A' && str[i] <= 'Z'))
-		{
 			return (0);
-		}
-	i++;
 	}
 	return (1);
 }
