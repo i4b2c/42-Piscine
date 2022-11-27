8e596d1d111b3868ce78fbe68b6928f45bdb84b4
i4b2c <117470349+i4b2c@users.noreply.github.com>
2022-11-27 05:48:46 +0000

    ✔️ OK ✔️

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -14,17 +14,11 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
 
-	i = 0;
-	while ((src[i] != '\0') && (i < n))
-	{
+	i = -1;
+	while ((src[++i] != '\0') && (i < n))
 		dest[i] = src[i];
-		i++;
-	}
 	while (i < n)
-	{
-		dest[i] = '\n';
-		i++;
-	}
+		dest[i++] = '\0';
 	return (dest);
 }
 /*
